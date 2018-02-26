a6dd5bd0f01806ce2e9349e3693829b2095f48be
Carter Duncan <dukeblue2017@gmail.com>
2018-02-26 15:40:52 -0600

    Fix USD value issue

--- bitschange-client/src/Balances.jsx
+++ bitschange-client/src/Balances.jsx
@@ -4,37 +4,51 @@ import React, { Component } from 'react';
 class Balances extends Component {
   constructor(props) {
     super(props);
-    console.log(props)
     this.state = {
       balanceArr: null,
       totalValue: null,
+      balanceObj: {
+        USD: { balance: this.props.balances.USD, usdValue: this.props.balances.USD, price: 1, denominator: 'USD'},
+        BTC: { balance: this.props.balances.BTC, usdValue: this.props.balances.BTC * this.props.prices.BTC.price, price: this.props.prices.BTC.price, denominator: 'USD'},
+        LTC: { balance: this.props.balances.LTC, usdValue: this.props.balances.LTC * this.props.prices.LTC.price * this.props.prices.BTC.price, price: this.props.prices.LTC.price, denominator: 'BTC'},
+        DOGE: { balance: this.props.balances.DOGE, usdValue: this.props.balances.DOGE * this.props.prices.DOGE.price * this.props.prices.BTC.price, price: this.props.prices.DOGE.price, denominator: 'BTC'},
+        XMR: { balance: this.props.balances.XMR, usdValue: this.props.balances.XMR * this.props.prices.XMR.price * this.props.prices.BTC.price, price: this.props.prices.XMR.price, denominator: 'BTC'}
+      },
     };
+    console.log(this.state);
   }
 
-  componentDidMount() {
+
+  componentWillMount() {
     this.calculateTotalValue();
     this.buildArr();
   }
 
   componentDidUpdate(prevProps) {
     if (JSON.stringify(prevProps.balances) !== JSON.stringify(this.props.balances)) {
-      this.calculateTotalValue();
+      this.setState({
+        balanceObj: {
+          USD: { balance: this.props.balances.USD, usdValue: this.props.balances.USD, price: 1, denominator: 'USD'},
+          BTC: { balance: this.props.balances.BTC, usdValue: this.props.balances.BTC * this.props.prices.BTC.price, price: this.props.prices.BTC.price, denominator: 'USD'},
+          LTC: { balance: this.props.balances.LTC, usdValue: this.props.balances.LTC * this.props.prices.LTC.price * this.props.prices.BTC.price, price: this.props.prices.LTC.price, denominator: 'BTC'},
+          DOGE: { balance: this.props.balances.DOGE, usdValue: this.props.balances.DOGE * this.props.prices.DOGE.price * this.props.prices.BTC.price, price: this.props.prices.DOGE.price, denominator: 'BTC'},
+          XMR: { balance: this.props.balances.XMR, usdValue: this.props.balances.XMR * this.props.prices.XMR.price * this.props.prices.BTC.price, price: this.props.prices.XMR.price, denominator: 'BTC'}
+        },
+        totalValue: this.state.balanceObj.USD.usdValue + this.state.balanceObj.BTC.usdValue + this.state.balanceObj.LTC.usdValue + this.state.balanceObj.DOGE.usdValue + this.state.balanceObj.XMR.usdValue,
+      });
       this.buildArr();
     }
   }
 
   buildArr() {
     if (this.props.balances) {
-      const keyArr = Object.keys(this.props.balances);
-      const balanceArr = keyArr.map(item => {
+      const keyArr = Object.keys(this.state.balanceObj);
+      const balanceArr = keyArr.map( (item) => {
         return (
           <div className="balance-entry" key={item}>
             <div className="which-coin">{item}</div>
-            {item === 'USD' && <div className="usd-value" />}
-            {item !== 'USD' && <div className="quantity">{this.props.balances[item].toFixed(5)}</div>}
-            {item === 'USD' && <div className="quantity">${this.props.balances[item].toFixed(2)}</div>}
-            {this.props.prices[item] && <div className="usd-value">${(this.props.balances[item] * this.props.prices[item].price).toFixed(2)}</div>}
-            {/*item !== 'USD' && <div className="percentage">{(this.props.balances[item] * this.props.prices[item].price)/this.state.totalValue}</div>*/}
+            <div className="quantity">{this.state.balanceObj[item].balance.toFixed(5)}</div>
+            <div className="usd-value">${this.state.balanceObj[item].usdValue.toFixed(2)}</div>
           </div>
         );
       });
@@ -43,13 +57,7 @@ class Balances extends Component {
   }
 
   calculateTotalValue() {
-    const currObj = {};
-    let totalCryptoValue = 0;
-    Object.keys(this.props.prices).forEach((item) => {
-      currObj[item] = this.props.balances[item] * this.props.prices[item].price;
-      totalCryptoValue += currObj[item];
-    });
-    const totalValue = totalCryptoValue + this.props.balances.USD;
+    const totalValue = this.state.balanceObj.USD.usdValue + this.state.balanceObj.BTC.usdValue + this.state.balanceObj.LTC.usdValue + this.state.balanceObj.DOGE.usdValue + this.state.balanceObj.XMR.usdValue;
     this.setState({ totalValue });
   }
 
@@ -57,7 +65,7 @@ class Balances extends Component {
     return (
       <div className="balance-container">
         My Balances:
-        {this.state.balanceArr && this.state.balanceArr}
+        {this.state.balanceArr}
         {this.state.totalValue &&
         <div className="total-value">
           Combined Value of All: {this.state.totalValue.toFixed(2)} USD
